bafc9485a209a355e0c27b4517b254913eacf1be
Victor Hempel-Mydtskov <victor@mydtskov.com>
2023-09-15 14:56:10 +0200

    added func get_all_files_in_dir

--- downloader.py
+++ downloader.py
@@ -3,41 +3,50 @@ import paramiko
 
 from utils import get_data_from_config
 
-provider = "epinion"  # "epinion" or "silverlining"
-
-# Server details
-hostname = get_data_from_config(provider, "hostname")
-port = get_data_from_config(provider, "port")
-username = get_data_from_config(provider, "username")
-password = get_data_from_config(provider, "password")  # Or use key-based authentication
-
-# paths
-remote_directory = get_data_from_config(provider, "remote_directory")
-remote_filename = get_data_from_config(provider, "remote_filename")
-local_directory = get_data_from_config(provider, "local_directory")
+providers = ["epinion", "silverlining"]
 
 
-# Establish SSH/SFTP connection
-transport = paramiko.Transport(
-    (hostname, int(port))
-)  # Port should be integer, but is treated as outputted as string in get_data_from_config
-transport.connect(
-    username=username, password=password
-)  # Replace with key-based authentication if needed
+def get_all_files_in_dir():
+    for filename in sftp.listdir():
+        remote_path = os.path.join(remote_directory, filename)
+        local_path = os.path.join(local_directory, filename)
+        get_file(remote_path, local_path)
 
-sftp = paramiko.SFTPClient.from_transport(transport)
 
-# Navigate to the remote directory
-sftp.chdir(remote_directory)
+def get_file(remote_path, local_path):
+    """Download a file from a remote server to the local machine."""
+    sftp.get(remote_path, local_path)
+    print(f"File downloaded from {remote_path} to {local_path}")
 
-# Download the file
-remote_path = os.path.join(remote_directory, remote_filename)
-local_path = os.path.join(local_directory, remote_filename)
 
-sftp.get(remote_path, local_path)
+for provider in providers:
+    # Server details
+    hostname = get_data_from_config(provider, "hostname")
+    port = get_data_from_config(provider, "port")
+    username = get_data_from_config(provider, "username")
+    password = get_data_from_config(
+        provider, "password"
+    )  # Or use key-based authentication
 
-# Close the SFTP connection
-sftp.close()
-transport.close()
+    # paths
+    remote_directory = get_data_from_config(provider, "remote_directory")
+    local_directory = get_data_from_config(provider, "local_directory")
 
-print(f"File downloaded from {remote_path} to {local_path}")
+    # Establish SSH/SFTP connection
+    transport = paramiko.Transport(
+        (hostname, int(port))
+    )  # Port should be integer, but is treated as outputted as string in get_data_from_config
+    transport.connect(
+        username=username, password=password
+    )  # Replace with key-based authentication if needed
+
+    sftp = paramiko.SFTPClient.from_transport(transport)
+
+    # Navigate to the remote directory
+    sftp.chdir(remote_directory)
+
+    get_all_files_in_dir()
+
+    # Close the SFTP connection
+    sftp.close()
+    transport.close()
